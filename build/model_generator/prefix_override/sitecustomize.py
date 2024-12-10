6c640bdbd91676a3bf487489ecd8879b8ea2aff2
hxrkpib <hxrkpib@foxmail.com>
2024-12-11 02:52:30 +0800

    update

--- build/model_generator/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/lizhen/works/code/OpenRobot/urdf_mjcf_autogenerator/install/model_generator'
